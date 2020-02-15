8ffdf8ab8ebd89a2f8618949509bab2da026936a
shinplest <shineceo97@naver.com>
2020-02-15 22:55:16 +0900

    Activity change structure

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/login/LoginActivity.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/login/LoginActivity.java
@@ -1,16 +1,22 @@
 package com.shinplest.airbnbclone.src.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
 import com.shinplest.airbnbclone.R;
 import com.shinplest.airbnbclone.src.BaseActivity;
+import com.shinplest.airbnbclone.src.main.MainActivity;
 
 import java.util.List;
 
 public class LoginActivity extends BaseActivity {
+
+    Button mBtnRegister;
 
 
     @Override
@@ -18,6 +24,15 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mBtnRegister = findViewById(R.id.btn_login_register_by_phone_number);
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
