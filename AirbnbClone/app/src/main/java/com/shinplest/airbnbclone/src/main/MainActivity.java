870438309d95d14e07d20e35f7bbd459023f628f
shinplest <shineceo97@naver.com>
2020-02-15 12:50:52 +0900

    finish splash Drawable

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/MainActivity.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/MainActivity.java
@@ -1,16 +1,23 @@
 package com.shinplest.airbnbclone.src.main;
 
-import androidx.appcompat.app.AppCompatActivity;
+//주요 고안점
+
+//splash화면에서 불필요한 시간을 없애기 위해서, drawable로 구현.
+
+
+
 
 import android.os.Bundle;
 
 import com.shinplest.airbnbclone.R;
+import com.shinplest.airbnbclone.src.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
     }
 }
