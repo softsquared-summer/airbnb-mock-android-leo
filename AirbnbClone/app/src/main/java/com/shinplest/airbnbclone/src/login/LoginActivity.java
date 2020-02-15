7ef0b2c47627241205d5b5b2f2d62f701f8b25b4
shinplest <shineceo97@naver.com>
2020-02-15 15:44:52 +0900

    spinner

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/login/LoginActivity.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/login/LoginActivity.java
@@ -1,16 +1,23 @@
 package com.shinplest.airbnbclone.src.login;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.Nullable;
 
 import com.shinplest.airbnbclone.R;
 import com.shinplest.airbnbclone.src.BaseActivity;
 
+import java.util.List;
+
 public class LoginActivity extends BaseActivity {
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
