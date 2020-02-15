ed48d6502d361d000346c2e6ff755af95a5bb15b
shinplest <shineceo97@naver.com>
2020-02-15 21:58:14 +0900

    explanation

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/MainActivity.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/MainActivity.java
@@ -3,6 +3,8 @@ package com.shinplest.airbnbclone.src.main;
 //주요 고안점
 
 //splash화면에서 불필요한 시간을 없애기 위해서, drawable로 구현.
+//로그인 액티비티가 필요할때만 나오기 위해서 엠티 액티비티로 분기를 해서 토큰유무에 따라 연결
+//fremelayout 대신 viewpager로 구현
 
 
 
