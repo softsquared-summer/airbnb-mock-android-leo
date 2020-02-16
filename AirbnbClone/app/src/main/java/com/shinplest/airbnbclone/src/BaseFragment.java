2bca41326347ba7628445c37cc806af6ae476480
shinplest <shineceo97@naver.com>
2020-02-16 11:41:25 +0900

    make base fragment

---
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/BaseFragment.java
@@ -0,0 +1,12 @@
+package com.shinplest.airbnbclone.src;
+
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+public class BaseFragment extends Fragment {
+
+    public void showCustomToastFrag(final String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+    }
+}
--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/FragSearch.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/FragSearch.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import com.shinplest.airbnbclone.R;
+import com.shinplest.airbnbclone.src.BaseFragment;
 
-public class FragSearch extends Fragment {
+public class FragSearch extends BaseFragment {
+
+    LinearLayout mLlSearch;
+
     public FragSearch() {
     }
 
@@ -19,6 +24,16 @@ public class FragSearch extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        mLlSearch = view.findViewById(R.id.ll_frag_search_search);
+        mLlSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCustomToastFrag("test");
+                //Toast.makeText(getActivity(), "test", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
 }
