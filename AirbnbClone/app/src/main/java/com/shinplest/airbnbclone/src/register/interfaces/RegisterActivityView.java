3f17ec794f20aa655714f5a05115a1adb9222aed
shinplest <shineceo97@naver.com>
2020-02-16 23:27:18 +0900

    before Test Login Api

---
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/register/interfaces/RegisterActivityView.java
@@ -0,0 +1,10 @@
+package com.shinplest.airbnbclone.src.register.interfaces;
+
+
+public interface RegisterActivityView {
+
+    void validateSuccess(String text);
+
+    void validateFailure(String message);
+}
+
