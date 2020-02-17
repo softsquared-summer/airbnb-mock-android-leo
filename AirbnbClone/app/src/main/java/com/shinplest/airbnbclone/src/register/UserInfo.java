64b395d6f321f1dfc33258540d5bcbed31abb1aa
shinplest <shineceo97@naver.com>
2020-02-17 14:54:41 +0900

    finish userinfo class

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/register/UserInfo.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/register/UserInfo.java
@@ -1,18 +1,43 @@
 package com.shinplest.airbnbclone.src.register;
 
 public class UserInfo {
-    String name;
-    String lastName;
-    String birthday;
-    String email;
-    String password;
+    private String phone;
+    private String last_name;
+    private String first_name;
+    private String birthday;
+    private String email;
+    private String pw;
 
-    public UserInfo(String name, String lastName, String birthday, String email, String password) {
-        this.name = name;
-        this.lastName = lastName;
+    public UserInfo(String phone, String last_name, String first_name, String birthday, String email, String pw) {
+        this.phone = phone;
+        this.last_name = last_name;
+        this.first_name = first_name;
         this.birthday = birthday;
         this.email = email;
-        this.password = password;
+        this.pw = pw;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPw() {
+        return pw;
+    }
 }
