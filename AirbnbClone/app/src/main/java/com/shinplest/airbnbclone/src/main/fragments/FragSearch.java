bdca15fbc33f31eb1730efeecc8846d8163adf86
shinplest <shineceo97@naver.com>
2020-02-19 10:03:42 +0900

    Search Design update

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragments/FragSearch.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragments/FragSearch.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
