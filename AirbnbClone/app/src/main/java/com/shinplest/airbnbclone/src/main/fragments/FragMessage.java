2ff26668c7c80288bd0443fb4ae9675d2f2cfa5e
shinplest <shinplest@gmail.com>
2020-02-21 23:32:19 +0900

    Addign menu on Fragment fail

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragments/FragMessage.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragments/FragMessage.java
@@ -2,6 +2,8 @@ package com.shinplest.airbnbclone.src.main.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -19,6 +21,17 @@ public class FragMessage extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message, container, false);
+        //새로고침 옵션메뉴 보이게
+        setHasOptionsMenu(true);
+
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.frag_message_menu, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+
     }
 }
