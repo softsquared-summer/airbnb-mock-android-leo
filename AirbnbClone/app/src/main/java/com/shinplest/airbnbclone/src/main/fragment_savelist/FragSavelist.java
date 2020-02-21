d0728e8dc818ba4745cf0dde19359884b606b1be
shinplest <shinplest@gmail.com>
2020-02-21 18:22:12 +0900

    Savelist recycler view upadte

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragment_savelist/FragSavelist.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragment_savelist/FragSavelist.java
@@ -28,9 +28,14 @@ public class FragSavelist extends Fragment {
         mRvSavelist = view.findViewById(R.id.rv_frag_savelist);
         mRvSavelist.setHasFixedSize(true);
         mRvSavelist.setLayoutManager(new LinearLayoutManager(getActivity()));
-        String[] roomPhotoUrlSet = {"https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/76Cz/image/LVlkZ5olVzCMIaEb7DqwmmE1Nxc.JPG", "https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/76Cz/image/LVlkZ5olVzCMIaEb7DqwmmE1Nxc.JPG"};
-        String[] locationSet = {"한국", "신주쿠, Shinjuku City, 일본"};
-        int[] roomsSet= {1,2};
+        String[] roomPhotoUrlSet = {"https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/76Cz/image/LVlkZ5olVzCMIaEb7DqwmmE1Nxc.JPG",
+                "https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/76Cz/image/LVlkZ5olVzCMIaEb7DqwmmE1Nxc.JPG",
+                "https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/76Cz/image/LVlkZ5olVzCMIaEb7DqwmmE1Nxc.JPG",
+                "https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/76Cz/image/LVlkZ5olVzCMIaEb7DqwmmE1Nxc.JPG",
+                "https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/76Cz/image/LVlkZ5olVzCMIaEb7DqwmmE1Nxc.JPG",
+                "https://t4.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/76Cz/image/LVlkZ5olVzCMIaEb7DqwmmE1Nxc.JPG"};
+        String[] locationSet = {"한국", "신주쿠, Shinjuku City, 일본", "한국", "신주쿠, Shinjuku City, 일본", "한국", "신주쿠, Shinjuku City, 일본"};
+        int[] roomsSet = {1, 2, 1, 2, 1, 2};
         mRvSavelist.setAdapter(new SavelistAdapter(roomPhotoUrlSet, locationSet, roomsSet));
 
         return view;
