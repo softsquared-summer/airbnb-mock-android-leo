47f8cfccb8965d7efa2ae761a8debd004a8ece95
shinplest <shinplest@gmail.com>
2020-02-24 23:59:42 +0900

    Before child fragment

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragment_search/CustomPageAdapter.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragment_search/CustomPageAdapter.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
