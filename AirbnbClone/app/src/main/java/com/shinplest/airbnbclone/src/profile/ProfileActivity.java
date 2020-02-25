35e2aaaea2a91c024dc8c4050cdacc63ee40bb12
shinplest <shinplest@gmail.com>
2020-02-26 00:24:31 +0900

    Before Request Modify Profile

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/profile/ProfileActivity.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/profile/ProfileActivity.java
@@ -1,8 +1,12 @@
 package com.shinplest.airbnbclone.src.profile;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -11,29 +15,54 @@ import com.shinplest.airbnbclone.R;
 import com.shinplest.airbnbclone.src.BaseActivity;
 import com.shinplest.airbnbclone.src.profile.interfaces.ProfileActivityView;
 import com.shinplest.airbnbclone.src.profile.models.ProfileResponse;
+import com.shinplest.airbnbclone.src.profile_modify.ProfileModifyActivity;
 
 public class ProfileActivity extends BaseActivity implements ProfileActivityView {
 
     private ProfileResponse.Result mProfile = null;
 
     private SimpleDraweeView mSvProfilePhoto;
+    private TextView mTvProfileTitle, mTvProfileRegisterDate;
+    private ImageView mIvModifyProfile;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        //가장먼저 ui 가져옴
         getUiSourse();
+
+
+        //프로필 수정으로 넘어감
+        mIvModifyProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, ProfileModifyActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         tryGetProfile();
     }
 
     private void getUiSourse() {
         mSvProfilePhoto = findViewById(R.id.sv_profile_profile_photo);
+        mTvProfileTitle = findViewById(R.id.tv_profile_info);
+        mTvProfileRegisterDate = findViewById(R.id.tv_profile_register_date);
+        mIvModifyProfile = findViewById(R.id.iv_profile_pen_modify);
     }
 
     private void UpdateUI() {
+        String imgUrl = mProfile.getProfileImgUrl();
+        String title = mProfile.getMyinfo();
+        String registerDate = mProfile.getCreatedDate();
+
         mSvProfilePhoto.setImageURI(Uri.parse(mProfile.getProfileImgUrl()));
+        if (title != null)
+            mTvProfileTitle.setText(title);
+        else mTvProfileTitle.setText("프로필 제목 없음");
+        mTvProfileRegisterDate.setText("회원 가입" + registerDate);
     }
 
     void tryGetProfile() {
