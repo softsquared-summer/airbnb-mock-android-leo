47a2887f606946fdf6d7d185ddaad6c3b621cfde
shinplest <shinplest@gmail.com>
2020-02-25 12:03:55 +0900

    forgive child fragment replace change gone visible

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/search/SearchActivity.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/search/SearchActivity.java
@@ -2,16 +2,23 @@ package com.shinplest.airbnbclone.src.search;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.shinplest.airbnbclone.R;
 import com.shinplest.airbnbclone.src.BaseActivity;
 
 public class SearchActivity extends BaseActivity {
 
-    TextView mTvCancel;
+    private Button testButton;
+    private LinearLayout mLlSearchTopContainer;
+    private ConstraintLayout mClSearchHouseContainer;
+
+    private TextView mTvCancel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +30,19 @@ public class SearchActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+
+        //검색화면 교체해주는 부분
+        mLlSearchTopContainer = findViewById(R.id.ll_search_top_container);
+        mClSearchHouseContainer = findViewById(R.id.cl_search_house_container);
+        testButton = findViewById(R.id.search_button_test);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLlSearchTopContainer.setVisibility(View.GONE);
+                mClSearchHouseContainer.setVisibility(View.VISIBLE);
             }
         });
     }
