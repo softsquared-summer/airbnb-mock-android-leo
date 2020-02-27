5467ef0a4f6c107f0af40b054def264f66174a20
shinplest <shinplest@gmail.com>
2020-02-27 12:16:08 +0900

    Update house Search design and data

--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/house/HouseActivity.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/house/HouseActivity.java
@@ -15,12 +15,12 @@ import com.shinplest.airbnbclone.src.house.models.HouseResponse;
 import com.shinplest.airbnbclone.src.search.HouseViewPageAdatper;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class HouseActivity extends BaseActivity implements HouseActivityView {
 
     private int mHouseNo;
     private ViewPager mVpHouseImages;
-
     private HouseResponse.Result mHouseData;
 
 
@@ -31,12 +31,12 @@ public class HouseActivity extends BaseActivity implements HouseActivityView {
             "https://pds.joins.com/news/component/htmlphoto_mmdata/201906/05/htm_20190605181513963994.jpg",
             "https://pds.joins.com/news/component/htmlphoto_mmdata/201906/05/htm_20190605181513963994.jpg"};
 
-
-
     ///ui
     private SimpleDraweeView mSvHouseHostProfile;
     private TextView mTvHouseTypeFirst, mTvHouseTitle, mTvHouseLocation,
-            mTvHouseHost, mTvhouseDetail;
+            mTvHouseHost, mTvhouseDetail, mTvHouseMinimumStay, mTvHouseFacilty1,
+            mTvHouseFacilty2, mTvHouseFacilty3, mTvHouseFacilty4, mTvHouseFacilty5,
+            mTvHouseFacilty6, mTvHouseCheckIn, mTvHouseCheckOut;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,12 +57,32 @@ public class HouseActivity extends BaseActivity implements HouseActivityView {
         //이미지
         ArrayList<HouseResponse.Image> houseDataImages = mHouseData.getImages();
         HouseResponse.Info houseDataInfo = mHouseData.getInfo();
+        ArrayList<HouseResponse.Facility> houseDataFacilities= mHouseData.getFacilities();
         mSvHouseHostProfile.setImageURI(Uri.parse(houseDataInfo.getHostImage()));
         mTvHouseTypeFirst.setText(houseDataInfo.getHouseType());
         mTvHouseTitle.setText(houseDataInfo.getHouseTitle());
         mTvHouseLocation.setText(houseDataInfo.getHouseLocation());
         mTvHouseHost.setText(houseDataInfo.getHouseHost());
         mTvhouseDetail.setText(houseDataInfo.getHouseDetail());
+        mTvHouseMinimumStay.setText(houseDataInfo.getMinimumStay());
+
+        //편의시설 크기만큼 랜덤으로 부여 해줌 -> 편의시설 6개만 랜럼으로 추출해서 보여주기 위함
+        int random = (int)(Math.random()*houseDataFacilities.size());
+        //범위밖으로 나가면
+        if(random+6 > houseDataFacilities.size()){
+            random -= houseDataFacilities.size();
+            random += 6;
+        }
+        mTvHouseFacilty1.setText(houseDataFacilities.get(random).getFacilitisename());
+        mTvHouseFacilty2.setText(houseDataFacilities.get(random+1).getFacilitisename());
+        mTvHouseFacilty3.setText(houseDataFacilities.get(random+2).getFacilitisename());
+        mTvHouseFacilty4.setText(houseDataFacilities.get(random+3).getFacilitisename());
+        mTvHouseFacilty5.setText(houseDataFacilities.get(random+4).getFacilitisename());
+        mTvHouseFacilty6.setText(houseDataFacilities.get(random+5).getFacilitisename());
+
+        mTvHouseCheckIn.setText(houseDataInfo.getCheckIn());
+        mTvHouseCheckOut.setText(houseDataInfo.getCheckOut());
+
     }
 
     private void getUiSourse() {
@@ -73,6 +93,15 @@ public class HouseActivity extends BaseActivity implements HouseActivityView {
         mTvHouseHost = findViewById(R.id.tv_house_host);
         mVpHouseImages = findViewById(R.id.vp_house);
         mTvhouseDetail = findViewById(R.id.tv_house_house_detail);
+        mTvHouseMinimumStay = findViewById(R.id.tv_house_house_minimum_stay);
+        mTvHouseFacilty1 = findViewById(R.id.tv_house_facility_1);
+        mTvHouseFacilty2 = findViewById(R.id.tv_house_facility_2);
+        mTvHouseFacilty3 = findViewById(R.id.tv_house_facility_3);
+        mTvHouseFacilty4 = findViewById(R.id.tv_house_facility_4);
+        mTvHouseFacilty5 = findViewById(R.id.tv_house_facility_5);
+        mTvHouseFacilty6 = findViewById(R.id.tv_house_facility_6);
+        mTvHouseCheckIn = findViewById(R.id.tv_house_checkin_time);
+        mTvHouseCheckOut = findViewById(R.id.tv_house_checkout_time);
     }
 
     void tryGetHouseInfo() {
