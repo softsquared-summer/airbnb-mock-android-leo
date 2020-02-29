ae5001199c4526e8edc280538bd4493733843286
shinplest <shinplest@gmail.com>
2020-02-29 13:45:24 +0900

    Save point

---
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragment_travel/ReservedHouseAdapter.java
@@ -0,0 +1,65 @@
+package com.shinplest.airbnbclone.src.main.fragment_travel;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.shinplest.airbnbclone.R;
+import com.shinplest.airbnbclone.src.main.fragment_travel.models.ReservedResponse;
+
+import java.util.ArrayList;
+
+public class ReservedHouseAdapter extends RecyclerView.Adapter<ReservedHouseAdapter.MyViewHolder> {
+    private ArrayList<ReservedResponse.ReservationList> mReservationLists;
+    private Context context;
+
+    public ReservedHouseAdapter(ArrayList<ReservedResponse.ReservationList> mReservationLists, Context context) {
+        this.mReservationLists = mReservationLists;
+        this.context = context;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public SimpleDraweeView mSvReservedHousePhoto;
+        public TextView mTvReservedDate, mTvReservedHouseName;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.mSvReservedHousePhoto = itemView.findViewById(R.id.sv_holder_reserved_house_img);
+            this.mTvReservedDate = itemView.findViewById(R.id.tv_holder_reserved_house_name);
+            this.mTvReservedHouseName = itemView.findViewById(R.id.tv_holder_reserved_date);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View holderView = LayoutInflater.from(parent.getContext()).inflate(R.layout.holder_reserved_house,parent,false);
+        MyViewHolder myViewHolder = new MyViewHolder(holderView);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        final ReservedResponse.ReservationList reservation = mReservationLists.get(position);
+        holder.mSvReservedHousePhoto.setImageURI(Uri.parse(reservation.getImageUrl()));
+        holder.mTvReservedHouseName.setText(reservation.getName());
+        holder.mTvReservedDate.setText(reservation.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mReservationLists.size();
+    }
+
+
+
+
+}
--- AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragment_travel/TravelFragment.java
+++ AirbnbClone/app/src/main/java/com/shinplest/airbnbclone/src/main/fragment_travel/TravelFragment.java
@@ -7,15 +7,26 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.shinplest.airbnbclone.R;
 import com.shinplest.airbnbclone.src.general.BaseFragment;
+import com.shinplest.airbnbclone.src.main.fragment_search.GridItemDecoration;
 import com.shinplest.airbnbclone.src.main.fragment_travel.interfaces.TravelFragmentView;
 import com.shinplest.airbnbclone.src.main.fragment_travel.models.ReservedResponse;
+
+import java.util.ArrayList;
 
 import static com.shinplest.airbnbclone.src.general.ApplicationClass.USER_NO;
 
 public class TravelFragment extends BaseFragment implements TravelFragmentView {
+
+    private RecyclerView mRvReservedHouse, mRvPastReservedHouse;
+
+    private ArrayList<ReservedResponse.ReservationList> mReservedHouseList;
+
+    private ReservedHouseAdapter mReservedHouseAdapter;
 
     public TravelFragment() {
     }
@@ -24,14 +35,20 @@ public class TravelFragment extends BaseFragment implements TravelFragmentView {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_travel, container, false);
-
         //getUiSource;
+        mRvReservedHouse = view.findViewById(R.id.rv_frag_travel_reserved_house);
+
+
+
+        mRvReservedHouse.setHasFixedSize(true);
+        mRvReservedHouse.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        mReservedHouseList = new ArrayList<>();
+        mReservedHouseAdapter = new ReservedHouseAdapter(mReservedHouseList,getContext());
+        mRvReservedHouse.setAdapter(mReservedHouseAdapter);
+        mRvReservedHouse.addItemDecoration(new GridItemDecoration(10));
 
         tryGetReservedHouses(USER_NO);
-
-
         return view;
-
     }
 
     void updateUi(){
@@ -50,6 +67,8 @@ public class TravelFragment extends BaseFragment implements TravelFragmentView {
         if (code == 100) {
             //널이아닐때만 해줘야함 데이터가 있을경우만 널익셉션 해주자
             showCustomToastFrag(result.getReservationList().get(0).getName());
+            mReservedHouseList.addAll(result.getReservationList());
+            mReservedHouseAdapter.notifyDataSetChanged();
         }
     }
 
